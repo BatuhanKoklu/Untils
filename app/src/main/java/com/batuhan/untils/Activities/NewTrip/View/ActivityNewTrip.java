4d4dd1950281dfd8a1d1e96047642b3c7e3d4c6d
Batuhan Koklu <batuhankoklu@hotmail.com>
2019-03-14 09:58:23 +0300

    Places autosearch eklenmeye çalışıldı.

--- app/src/main/java/com/batuhan/untils/Activities/NewTrip/View/ActivityNewTrip.java
+++ app/src/main/java/com/batuhan/untils/Activities/NewTrip/View/ActivityNewTrip.java
@@ -8,12 +8,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.batuhan.untils.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ActivityNewTrip extends Fragment {
+
+    private  PlaceAutocompleteFragment placeAutocompleteFragment;
+    private GoogleMap mMap;
+    Marker marker;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.activity_new_trip, container, false);
+
+
 
 
         return view;
